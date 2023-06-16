79bb57985c90eca980f93bd88caa7ea855a001cb
ujwxl23 <ujwalkr23@gmail.com>
2023-06-16 14:26:32 +0530

    errorCreateProvideResolved

--- Project/contracts/FileShare.sol
+++ Project/contracts/FileShare.sol
@@ -7,20 +7,19 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FileShare is ERC20, Ownable {
-
-    constructor () ERC20("shareToken", "STK" ) {
-         _mint(msg.sender, 50 * 10 ** decimals());
+    constructor() ERC20("shareToken", "STK") {
+        _mint(msg.sender, 50 * 10 ** decimals());
     }
 
-    struct Request{
+    struct Request {
         string description;
         address payable reqProvider;
         uint256 needIdDevice;
     }
-    mapping (uint=>Request) public requests;
+    mapping(uint => Request) public requests;
     uint256 public ReqId;
 
-    struct Provide{
+    struct Provide {
         string[] description;
         string[] deviceName;
         address payable recipient;
@@ -28,72 +27,96 @@ contract FileShare is ERC20, Ownable {
         uint256 timestamp;
         bool[] engage;
     }
-    mapping (uint256=>Provide) public providers;
+    mapping(uint256 => Provide) public providers;
     uint256 public ProId;
 
     uint256 public count;
 
-    function createProvide(string memory _description, string memory _deviceName, uint256 _space) public {
+    function createProvide(
+        string memory _description,
+        string memory _deviceName,
+        uint256 _space
+    ) public payable {
         Provide storage newProvide = providers[ProId];
         ProId++;
-        count=0;
-        newProvide.description[count]=_description;
-        newProvide.deviceName[count]=_deviceName;
-        newProvide.recipient=payable(msg.sender);
-        newProvide.space[count]=_space;
-        newProvide.engage[count]=false;
+        count = 0;
+        newProvide.description.push(_description);
+        newProvide.deviceName.push(_deviceName);
+        newProvide.recipient = payable(msg.sender);
+        newProvide.space.push(_space);
+        newProvide.engage.push(false);
     }
 
-    function addDevicesByProvider(uint256 _proId, string memory _newDeviceName, uint256 _newspace, string memory _newDescription) public {
-        Provide storage thisProvide=providers[_proId];
+    function addDevicesByProvider(
+        uint256 _proId,
+        string memory _newDeviceName,
+        uint256 _newspace,
+        string memory _newDescription
+    ) public {
+        Provide storage thisProvide = providers[_proId];
         ++count;
-        thisProvide.description[count]=_newDescription;
-        thisProvide.deviceName[count]=_newDeviceName;
-        thisProvide.space[count]=_newspace;
-        thisProvide.engage[count]=false;
+        thisProvide.description.push(_newDescription);
+        thisProvide.deviceName.push(_newDeviceName);
+        thisProvide.space.push(_newspace);
+        thisProvide.engage.push(false);
     }
 
-    function makeRequestToProvider(uint256 _proId, uint256 _spaceReq, string  calldata _deviceNameReq) public {
+    function makeRequestToProvider(
+        uint256 _proId,
+        uint256 _spaceReq,
+        string calldata _deviceNameReq
+    ) public {
         // require(raisedAmt>=targetprice);
-        Provide storage thisProvide=providers[_proId];
+        Provide storage thisProvide = providers[_proId];
         uint256 i;
-        string storage str=(thisProvide.deviceName)[i];
-        for (i=0; i < (thisProvide.deviceName).length; i++){
-            if ( compare(str,_deviceNameReq)){
-                require(thisProvide.space[i] >= _spaceReq ,"The provider does not have enoungh space left.");
-                require(thisProvide.engage[i]==false,"The provider is already busy, no resources available.");
+        string storage str = (thisProvide.deviceName)[i];
+        for (i = 0; i < (thisProvide.deviceName).length; i++) {
+            if (compare(str, _deviceNameReq)) {
+                require(
+                    thisProvide.space[i] >= _spaceReq,
+                    "The provider does not have enoungh space left."
+                );
+                require(
+                    thisProvide.engage[i] == false,
+                    "The provider is already busy, no resources available."
+                );
                 //transferToken()
-                thisProvide.engage[i]=true;
-                thisProvide.timestamp=block.timestamp;
+                thisProvide.engage[i] = true;
+                thisProvide.timestamp = block.timestamp;
                 Request storage newRequest = requests[ReqId];
                 ReqId++;
-                newRequest.description=_deviceNameReq;
-                newRequest.reqProvider=thisProvide.recipient;
-                newRequest.needIdDevice=_proId;
+                newRequest.description = _deviceNameReq;
+                newRequest.reqProvider = thisProvide.recipient;
+                newRequest.needIdDevice = _proId;
             }
         }
     }
 
-    function getAllAvailableDevices(uint256 _proIdAvailable) public view returns(string[] memory availableDevices){
-        Provide storage thisProvide=providers[_proIdAvailable];
+    function getAllAvailableDevices(
+        uint256 _proIdAvailable
+    ) public view returns (string[] memory availableDevices) {
+        Provide storage thisProvide = providers[_proIdAvailable];
         uint index;
-        for (uint256 i=0; i < (thisProvide.deviceName).length; i++){
-            if (thisProvide.engage[i]){
-
-                availableDevices[index]=(thisProvide.deviceName[i]);
+        for (uint256 i = 0; i < (thisProvide.deviceName).length; i++) {
+            if (thisProvide.engage[i]) {
+                availableDevices[index] = (thisProvide.deviceName[i]);
             }
         }
     }
 
-    function compare(string memory str1, string memory str2) public pure returns (bool) {
+    function compare(
+        string memory str1,
+        string memory str2
+    ) public pure returns (bool) {
         if (bytes(str1).length != bytes(str2).length) {
             return false;
         }
-        return keccak256(abi.encodePacked(str1)) == keccak256(abi.encodePacked(str2));
+        return
+            keccak256(abi.encodePacked(str1)) ==
+            keccak256(abi.encodePacked(str2));
     }
 
     // function finishUse();
     // function approveRequestorUse();
     // function proStop();
-
 }
